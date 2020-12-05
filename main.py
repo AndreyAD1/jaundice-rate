61120aa041308377b494e331e2cc96814bdb1f49
AndreyAD <arakelyants.andrey@yandex.ru>
2020-12-05 11:25:23 +0300

    Get the first rate

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import aiohttp
 import asyncio
 
+import aiohttp
+import pymorphy2
+
 from adapters import SANITIZERS
+from text_tools import split_by_words, calculate_jaundice_rate
 
 
 async def fetch(session, url):
@@ -14,7 +17,12 @@ async def main():
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, 'https://inosmi.ru/social/20201205/248649230.html')
         plain_text = SANITIZERS['inosmi_ru'](html, plaintext=True)
-        print(plain_text)
+        morph = pymorphy2.MorphAnalyzer()
+        text_words = split_by_words(morph, plain_text)
+        key_words = ['одноклассники', 'исключить', 'неправильный']
+        jaundice_rate = calculate_jaundice_rate(text_words, key_words)
+        print('Рейтинг: ', jaundice_rate)
+        print('Слов в статье: ', len(text_words))
 
 
 asyncio.run(main())
